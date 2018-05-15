79f5d3e2d41634d7ef26f0976ee6867656ac5306
AhmedAliEldiasty <AhmedEldiasty565@gmail.com>
2018-05-15 12:03:42 +0200

    subject information v1

--- project/app/src/main/java/com/example/ahmedel_diasty/mas/InnerRecyclerAdapter.java
+++ project/app/src/main/java/com/example/ahmedel_diasty/mas/InnerRecyclerAdapter.java
@@ -1,12 +1,16 @@
 package com.example.ahmedel_diasty.mas;
 
 
+import android.app.Dialog;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ahmedel_diasty.mas.Interface.ItemClickListener;
 
 /**
  * Created by Ahmed El-Diasty on 24/03/2018.
@@ -19,29 +23,55 @@ import android.widget.TextView;
     public InnerRecyclerAdapter(Context context) {
         this.context = context;
     }
+    Dialog dialog;
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row,parent,false);
+
         return new InnerRecyclerAdapter.MyViewHolder(view);
+
     }
+
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         holder.lectureName.setText(lectuers[position]);
+        dialog = new Dialog(context.getApplicationContext());
+        dialog.setContentView(R.layout.subject_information);
+        holder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(context, "The index number "+position, Toast.LENGTH_SHORT).show();
+                dialog.show();
+                notifyDataSetChanged();
+            }
+        });
     }
+
 
     @Override
     public int getItemCount() {
         return lectuers.length;
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView lectureName;
+
+        ItemClickListener itemClickListener;
+
+        public void setItemClickListener(ItemClickListener itemClickListener){
+            this.itemClickListener = itemClickListener;
+        }
 
         public MyViewHolder(View itemView) {
             super(itemView);
             lectureName = (TextView)itemView.findViewById(R.id.nameTxt);
+            itemView.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View view) {
+            itemClickListener.onClick(view,getAdapterPosition());
         }
     }
 }
---
+++ project/app/src/main/java/com/example/ahmedel_diasty/mas/Interface/ItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.ahmedel_diasty.mas.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+
+    void onClick(View view, int position);
+}
